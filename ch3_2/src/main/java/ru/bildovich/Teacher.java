679b8d390fd4fbbce7dcbb205b4bd644542731e5
hootor <bildovich@mail.ru>
2017-03-01 00:06:50 +1000

    add 6

--- ch3_2/src/main/java/ru/bildovich/Teacher.java
+++ ch3_2/src/main/java/ru/bildovich/Teacher.java
@@ -1,23 +1,46 @@
 package ru.bildovich;
 
 /**
- * bildovich
- * 25.02.2017.
+ * Class Teacher.
+ *
+ * @author bildovich.
+ * @version 1.0.
  */
 public class Teacher extends Profession {
 
+    /**
+     * The collection pupils.
+     */
     private MyList pupils;
+
+    /**
+     * The collection lesson schedule.
+     */
     private LessonSchedule lessonSchedule;
+
+    /**
+     * The collection specializations.
+     */
     private MyList specialization;
 
+    /**
+     * Method to add pupil.
+     *
+     * @param namePupil the name.
+     */
     public void addPupil(String namePupil) {
 
         pupils.add(new Pupil(namePupil));
 
     }
 
+    /**
+     * Method to delet pupil.
+     *
+     * @param pupilName the name.
+     */
     public void deletPupils(String pupilName) {
-        for (int i = 0; i < pupils.size(); ) {
+        for (int i = 0; i < pupils.size();) {
             if (pupils.get(i).getName().equals(pupilName)) {
                 pupils.remove(i);
             } else {
@@ -26,14 +49,27 @@ public class Teacher extends Profession {
         }
     }
 
+    /**
+     * Setter method for name lessonSchedule.
+     *
+     * @param lessonSchedule the lessonSchedule field.
+     */
     public void setLessonSchedule(LessonSchedule lessonSchedule) {
         this.lessonSchedule = lessonSchedule;
     }
 
-    public void СonductLesson() {
+    /**
+     * Method for Lesson.
+     */
+    public void conductLesson() {
 
     }
 
+    /**
+     * Method add new specialization.
+     *
+     * @param nameSpecialization the name of the new specialization.
+     */
     public void receiveSpecialization(String nameSpecialization) {
         specialization.add(new TeacherSpecialization(nameSpecialization));
     }
--- ch3_2/src/main/java/ru/bildovich/TeacherSpecialization.java
+++ ch3_2/src/main/java/ru/bildovich/TeacherSpecialization.java
@@ -1,17 +1,28 @@
 package ru.bildovich;
 
 /**
- * bildovich
- * 28.02.2017.
+ * @author bildovich.
+ * @version 1.0.
  */
 public class TeacherSpecialization implements Specialization, Specifics {
 
+    /**
+     * The name field.
+     */
     private String name;
 
+    /**
+     * Constructor of TeacherSpecialization class.
+     * @param name the name.
+     */
     public TeacherSpecialization(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter method for name field.
+     * @return name.
+     */
     @Override
     public String getName() {
         return name;
