5f39c5f25a122d1c919c98c614a048ee407b0ea5
hootor <bildovich@mail.ru>
2017-02-28 23:57:20 +1000

    add 6

--- ch3_2/src/main/java/ru/bildovich/Pupil.java
+++ ch3_2/src/main/java/ru/bildovich/Pupil.java
@@ -2,17 +2,29 @@ package ru.bildovich;
 
 /**
  * Class Pupil.
+ *
  * @author bildovich.
  * @version 1.0.
  */
-public class Pupil implements Specifics{
+public class Pupil implements Specifics {
 
+    /**
+     * The name field.
+     */
     private String name;
 
+    /**
+     * Constructor of Pupil class.
+     * @param name the name.
+     */
     public Pupil(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter method for name field.
+     * @return name.
+     */
     public String getName() {
         return name;
     }
--- ch3_2/src/main/java/ru/bildovich/Specifics.java
+++ ch3_2/src/main/java/ru/bildovich/Specifics.java
@@ -5,5 +5,10 @@ package ru.bildovich;
  * 28.02.2017.
  */
 public interface Specifics {
+
+    /**
+     * Getter method for name field.
+     * @return name.
+     */
     String getName();
 }
